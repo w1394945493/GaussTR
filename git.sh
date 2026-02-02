d8d70fd3c180799f14dd06c223ccd1b3a435c30b
wys <1394945493@qq.com>
2026-02-02 08:02:32 +0000

    update git

--- git.sh
+++ git.sh
@@ -2,3 +2,13 @@ git checkout -m -b experiment
 git add .
 git commit -m ''
 git push origin experiment
+
+# 断开故障的 VS Code 凭证助手
+unset GIT_ASKPASS
+unset SSH_ASKPASS
+
+# 提交代码 
+git push origin experiment
+
+# 开启凭证存储
+git config --global credential.helper store
